4b9cd966c794899529afdd2e962ecaf8ad09c0e1
asc4nio <ascaniopaonetti@gmail.com>
2023-01-21 23:17:59 +0100

    textHover

--- a-star2d-02/sketch.js
+++ a-star2d-02/sketch.js
@@ -1,30 +1,41 @@
 let stars = [];
-let source = [];
+let textures = [];
 
-let normMouse
+let normMouse, starScale
 
-let starScale;
-const quantityMult = 1;
+const quantityMult = 2;
+
+const content = [
+  {
+    string: 'wotcha',
+    imageURL: 'images/wotcha-desktop.png'
+  },
+  {
+    string: 'prototype',
+    imageURL: 'images/me-desktop.png'
+  },
+  {
+    string: 'aequitas',
+    imageURL: 'images/aequitas-desktop.png'
+  },
+  {
+    string: 'our planet',
+    imageURL: 'images/tdh-desktop.png'
+  },
+  {
+    string: 'website',
+    imageURL: 'images/cadiai-mobile.png'
+  },
+  {
+    string: 'mobile',
+    imageURL: 'images/tdh-mobile.png'
+  },
+]
 
 function preload() {
-  source[0] = loadImage(
-    "images/wotcha-desktop.png"
-  );
-  source[1] = loadImage(
-    "images/me-desktop.png"
-  );
-  source[2] = loadImage(
-    "images/aequitas-desktop.png"
-  );
-  source[3] = loadImage(
-    "images/tdh-desktop.png"
-  );
-  source[4] = loadImage(
-    "images/cadiai-mobile.png"
-  );
-  source[5] = loadImage(
-    "images/tdh-mobile.png"
-  );
+  for (let i = 0; i < content.length; i++) {
+    textures[i] = loadImage(content[i].imageURL)
+  }
 }
 
 function setStarScale() {
@@ -41,8 +52,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   normMouse = createVector()
-  
-  for (let i = 0; i < source.length * quantityMult; i++) {
+
+  for (let i = 0; i < content.length * quantityMult; i++) {
     stars.push(new Star());
   }
   setStarScale();
@@ -54,10 +65,10 @@ function draw() {
 
   translate(width / 2, height / 2);
 
-  normMouse.x = mouseX - innerWidth/2
-  normMouse.y = mouseY - innerHeight/2
+  normMouse.x = mouseX - innerWidth / 2
+  normMouse.y = mouseY - innerHeight / 2
 
-  for(let star of stars){
+  for (let star of stars) {
     star.show()
     star.update()
     star.mouseIntersects(normMouse)
@@ -70,9 +81,9 @@ class Star {
   constructor() {
     this.x = random(-width, width);
     this.y = random(-height, height);
-    this.scale = random(0,0.5);
+    this.scale = random(0, 0.5);
     this.increment = random(0.0005, 0.002);
-    this.index = floor(random(source.length));
+    this.index = floor(random(content.length));
 
     this.centerPosition = createVector()
 
@@ -84,15 +95,15 @@ class Star {
     noStroke();
 
     push();
-    
+
     scale(this.scale);
 
     image(
-      source[this.index],
+      textures[this.index],
       this.x,
       this.y,
-      source[this.index].width * starScale,
-      source[this.index].height * starScale
+      textures[this.index].width * starScale,
+      textures[this.index].height * starScale
     );
 
     if (this.scale < 0.15) {
@@ -101,8 +112,8 @@ class Star {
       rect(
         this.x,
         this.y,
-        source[this.index].width * starScale,
-        source[this.index].height * starScale
+        textures[this.index].width * starScale,
+        textures[this.index].height * starScale
       );
     } else if (this.scale >= 0.98) {
       this.alpha = map(this.scale, 0.98, 1, 0, 255);
@@ -110,8 +121,8 @@ class Star {
       rect(
         this.x,
         this.y,
-        source[this.index].width * starScale,
-        source[this.index].height * starScale
+        textures[this.index].width * starScale,
+        textures[this.index].height * starScale
       );
     }
     pop();
@@ -122,43 +133,44 @@ class Star {
     // collision variables
     this.bbP1.x = this.x
     this.bbP1.y = this.y
-    this.bbP2.x = this.x + (source[this.index].width * starScale)
-    this.bbP2.y = this.y + (source[this.index].height * starScale)
+    this.bbP2.x = this.x + (textures[this.index].width * starScale)
+    this.bbP2.y = this.y + (textures[this.index].height * starScale)
     this.bbP1.mult(this.scale)
     this.bbP2.mult(this.scale)
 
-    this.centerPosition.x = (this.bbP1.x+this.bbP2.x)/2
-    this.centerPosition.y = (this.bbP1.y+this.bbP2.y)/2
+    this.centerPosition.x = (this.bbP1.x + this.bbP2.x) / 2
+    this.centerPosition.y = (this.bbP1.y + this.bbP2.y) / 2
 
     // reset cycle
     if (this.scale > 1) {
-      this.index = floor(random(source.length));
+      this.index = floor(random(content.length));
       this.scale = 0;
       this.x = random(-width, width);
       this.y = random(-height, height);
     }
 
     // debug BB
-    push()
-      fill(0,255,0)
-      circle(this.bbP1.x,this.bbP1.y,10)
-      circle(this.bbP2.x,this.bbP2.y,10)
-      fill(255,0,0)
-      circle(this.centerPosition.x,this.centerPosition.y,10)
-      fill(0,0,255)
-      circle(normMouse.x, normMouse.y, 10)
-    pop()
+    // push() 
+    // fill(0, 255, 0) // bbP1 bbP2
+    // circle(this.bbP1.x, this.bbP1.y, 10)
+    // circle(this.bbP2.x, this.bbP2.y, 10)
+    // fill(255, 0, 0) //centerP
+    // circle(this.centerPosition.x, this.centerPosition.y, 10)
+    // fill(0, 0, 255) //mouseP
+    // circle(normMouse.x, normMouse.y, 10)
+    // pop()
   }
-  mouseIntersects(_v2Input){
-
-    if(_v2Input.x > this.bbP1.x &&
-      _v2Input.x < this.bbP2.x &&
-      _v2Input.y > this.bbP1.y &&
-      _v2Input.y < this.bbP2.y
-      ){
-console.log('intersection')
-    } else {
-      // console.log('no intersection')
+  mouseIntersects(_mouse) {
+    if (_mouse.x > this.bbP1.x &&
+      _mouse.x < this.bbP2.x &&
+      _mouse.y > this.bbP1.y &&
+      _mouse.y < this.bbP2.y
+    ) {
+      push()
+      textSize(32);
+      fill(255, 0, 0)
+      text(content[this.index].string, this.centerPosition.x, this.centerPosition.y)
+      pop()
     }
 
   }
