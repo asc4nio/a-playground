2487c8139e12c0566bbd51ac8e56c4d603e2a8e0
asc4nio <ascaniopaonetti@gmail.com>
2023-01-28 14:08:21 +0100

    animating

--- a-p5-renderer-01/sketch.js
+++ a-p5-renderer-01/sketch.js
@@ -1,12 +1,21 @@
 const density = "987654321"
 let inputImg
 
+let renderer = {
+  cornerRadius: 0,
+  cornerRadiusMax : 16
+}
+
+let cornerAnim = gsap.timeline({repeat:-1, onUpdate: () => {console.log(renderer.cornerRadius)}})
+cornerAnim.to(renderer,{cornerRadius:0, duration:1, ease:'none'},'0')
+cornerAnim.to(renderer,{cornerRadius:renderer.cornerRadiusMax, duration:1, ease:'none'},'>')
+
 function preload(){
   inputImg = loadImage('asset/inputTest.jpg')
 }
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(innerWidth,innerHeight);
 }
 
 function draw() {
@@ -15,6 +24,7 @@ background(0)
 
 let w = width / inputImg.width;
 let h = height / inputImg.height;
+
 inputImg.loadPixels();
 
 for (let i = 0; i < inputImg.width; i++) {
@@ -27,7 +37,8 @@ for (let i = 0; i < inputImg.width; i++) {
     
     noStroke();
     fill(avg);
-    square(i * w, j * h, w);
+    // circle(i * w, j * h, w);
+    rect(i * w, j * h, w,h, renderer.cornerRadius,renderer.cornerRadius,renderer.cornerRadius,renderer.cornerRadius)
     
     // const len = density.length;
     // const charIndex = floor(map(avg,0,255,len,0));
@@ -37,7 +48,7 @@ for (let i = 0; i < inputImg.width; i++) {
     // text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
     
   }
-}
+} 
 }
 
 // let w = width / inputImg.width;
