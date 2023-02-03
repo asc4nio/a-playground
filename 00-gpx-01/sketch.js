028b1155feeeae6f340f9ab916dfe462b65dd8b3
asc4nio <ascaniopaonetti@gmail.com>
2023-02-03 23:29:43 +0100

    gpx staticMap

--- 00-gpx-01/sketch.js
+++ 00-gpx-01/sketch.js
@@ -1,7 +1,24 @@
 /**https://docs.maptiler.com/maplibre-gl-js/tutorials/ */
 
+/*
+const key = 'cSp27W2Q0Ud3EyJfNsz2';
 
+var bounds = [
+  [11.32, 44.48], // Southwest coordinates
+  [11.28, 44.52] // Northeast coordinates
+];
 
+const map2 = new maplibregl.Map({
+  container: 'map', // container's id or the HTML element in which MapLibre GL JS will render the map
+  style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${key}`, // style URL
+  center: [11.342976, 44.496441], // starting position [lng, lat]
+  zoom: 14, // starting zoom
+  maxBounds: bounds // Sets bounds as max
+});
+
+map.fitBounds(bounds);
+*/
+///////////////////////////////////////////////////////////////
 
 var gpx = new gpxParser();
 
@@ -22,26 +39,15 @@ fetch('asset/percorso.gpx')
     console.log(totalDistance)
   })
 
+  let mapImage
+  function preload(){
+    mapImage = loadImage('asset/map.png')
+  }
 function setup() {
   createCanvas(innerWidth, innerHeight);
   clear()
 
-  const key = 'cSp27W2Q0Ud3EyJfNsz2';
-
-var bounds = [
-  [11.37, 44.44], // Southwest coordinates
-  [11.43, 44.41] // Northeast coordinates
-];
-
-const map = new maplibregl.Map({
-  container: 'map', // container's id or the HTML element in which MapLibre GL JS will render the map
-  style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${key}`, // style URL
-  center: [11.342976, 44.496441], // starting position [lng, lat]
-  zoom: 14, // starting zoom
-  maxBounds: bounds // Sets bounds as max
-});
-// map.addControl(new maplibregl.NavigationControl(), 'top-right');
-
+  image(mapImage,0,0,width,height)
 
 }
 
@@ -52,8 +58,11 @@ function draw() {
     let lat= gpx.tracks[0].points[i].lat
     let lon = gpx.tracks[0].points[i].lon
 
-    let mapLat = map(lat,44.41,44.44,0,width)
-    let mapLon = map(lon,11.37,11.43,0,height)
+    // let mapLat = map(lat,44.41,44.44,0,width)
+    // let mapLon = map(lon,11.37,11.43,0,height)
+
+    let mapLat = map(lat,44.4102,44.44,0,width)
+    let mapLon = map(lon,11.3700,11.4297,0,height)
 
     vertex(mapLat,mapLon)
     push()
