4f6bd32f76631a0a88546471bd5027ac306a5822
asc4nio <ascaniopaonetti@gmail.com>
2023-02-03 22:57:46 +0100

    overlay wip

--- 00-gpx-01/sketch.js
+++ 00-gpx-01/sketch.js
@@ -1,3 +1,8 @@
+/**https://docs.maptiler.com/maplibre-gl-js/tutorials/ */
+
+
+
+
 var gpx = new gpxParser();
 
 let parsedData
@@ -10,26 +15,54 @@ fetch('asset/percorso.gpx')
     gpx.parse(data)
     console.log(gpx)
 
+    let geoJSON = gpx.toGeoJSON();
+    console.log(geoJSON)
+
     var totalDistance = gpx.tracks[0].distance.total;
     console.log(totalDistance)
   })
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
+  clear()
+
+  const key = 'cSp27W2Q0Ud3EyJfNsz2';
+
+var bounds = [
+  [11.37, 44.44], // Southwest coordinates
+  [11.43, 44.41] // Northeast coordinates
+];
+
+const map = new maplibregl.Map({
+  container: 'map', // container's id or the HTML element in which MapLibre GL JS will render the map
+  style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${key}`, // style URL
+  center: [11.342976, 44.496441], // starting position [lng, lat]
+  zoom: 14, // starting zoom
+  maxBounds: bounds // Sets bounds as max
+});
+// map.addControl(new maplibregl.NavigationControl(), 'top-right');
 
 
 }
 
 function draw() {
+  beginShape()
+  noFill()
   for(let i=0; i<gpx.tracks[0].points.length; i++){
     let lat= gpx.tracks[0].points[i].lat
     let lon = gpx.tracks[0].points[i].lon
 
-    let mapLat = map(lat,44.4,44.5,0,width)
-    let mapLon = map(lon,11.3,11.5,0,height)
+    let mapLat = map(lat,44.41,44.44,0,width)
+    let mapLon = map(lon,11.37,11.43,0,height)
 
-    circle(mapLat,mapLon,4)
+    vertex(mapLat,mapLon)
+    push()
+      noStroke()
+      fill(255)
+      circle(mapLat,mapLon,4)
+    pop()
   }
+  endShape()
 
 }
 
